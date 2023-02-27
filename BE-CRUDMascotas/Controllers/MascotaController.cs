c90a8332867ae60b0a53864c43c1acc4477ceb17
Belén López <belop_18@hotmail.com>
2023-02-26 23:42:19 -0300

    Editar Mascota

--- BE-CRUDMascotas/Controllers/MascotaController.cs
+++ BE-CRUDMascotas/Controllers/MascotaController.cs
@@ -84,7 +84,40 @@ namespace BE_CRUDMascotas.Controllers
                 _context.Add(mascota);
                 await _context.SaveChangesAsync();
 
-                return CreatedAtAction("Get", new {id = mascota.Id}, mascota);
+                return CreatedAtAction("Get", new { id = mascota.Id }, mascota);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, Mascota mascota)
+        {
+            try
+            {
+                if (id != mascota.Id)
+                {
+                    return BadRequest();
+                }
+
+                var mascotaItem = await _context.Mascotas.FindAsync(id);
+
+                if (mascotaItem == null)
+                {
+                    return NotFound();
+                }
+
+                mascotaItem.Nombre = mascota.Nombre;
+                mascotaItem.Raza = mascota.Raza;
+                mascotaItem.Edad = mascota.Edad;
+                mascotaItem.Peso = mascota.Peso;
+                mascotaItem.Color = mascota.Color;
+
+                await _context.SaveChangesAsync();
+
+                return NoContent();
             }
             catch (Exception ex)
             {
